469df1743e045cbbc841d786f9c832a2cb213545
Annikeyli <anna20061010@yandex.ru>
2025-05-29 18:04:42 +0300

    Update practica2.cpp

--- practica2.cpp
+++ practica2.cpp
@@ -82,81 +82,21 @@ int main() {
 
     int index = searchHashTable(hashTable, day, month, year);
     if (index != -1) {
-        cout << index << ":\n";
-        cout << arr[index] << "\n";
+        //cout << index << ":\n";
+        //cout << arr[index] << "\n";
+
+        // Чтение напрямую из бинарного файла
+        ifstream inbin("a.bin", ios_base::binary);
+        inbin.seekg(index * sizeof(Znak));
+        Znak z;
+        inbin.read((char*)&z, sizeof(Znak));
+        cout << "\n from bin file: \n";
+        cout << z << "\n";
     }else {
         cout << "\n No \n";
     }
 
     return 0;
 }
-
-//#include <iostream>
-//#include <fstream>
-//#include <ios>
-//#include <string.h>
-//#include "ZNAK.h"
-//#include "My_Array.h"
-////#include "MyList.h" 
-//using namespace std;
-//void printArr(int* a, int n) {
-//    for (int i = 0; i < n; i++)
-//        cout << a[i] << " ";
-//}
-//
-//const int m = 10;
-//void createHashTable(int hashTable[][2], int in[6][2], int n) {
-//    int j;
-//    for (int i = 0; i < m; i++)hashTable[i][0] = -1;
-//    for (int i = 0; i < n; i++) {
-//        int i1 = 0;
-//        int k = in[i][0];
-//        while (true) {
-//            j = (k + i1 * i1) % m;
-//            if (hashTable[j][0] == -1)break;
-//            i1++;
-//        }
-//        hashTable[j][0] = k;
-//        hashTable[j][1] = in[i][1];
-//    }
-//}
-//int search(int hashTable[][2], int key) {
-//    int i1 = 0;
-//    int j;
-//    while (true) {
-//        j = (key + i1 * i1) % m;
-//        if (hashTable[j][0] == key)break;
-//        i1++;
-//    }
-//
-//    return  hashTable[j][1];
-//}
-//
-//int main()
-//{    
-//    My_Array<Znak> arr;
-//    ifstream in("text.txt");
-//    if (!in)cout << "\nfile not open\n";
-//    in >> arr;
-//    cout << arr;
-//    ofstream outbin("a.bin", ios_base::binary);
-//    arr.writeBin(outbin);
-//    ifstream inbin("a.bin", ios_base::binary);
-//    
-//
-//    int in[6][2] = { 2002,0,1914,1,1939,2,1945,3,1953,4,1944,5 };
-//    for (int i = 0; i < 6; i++) {
-//        cout << in[i][0] << " " << in[i][1] << "\n";
-//    }
-//    cout << "\n";
-//    int hashTable[m][2];
-//    createHashTable(hashTable, in, 6);
-//    for (int i = 0; i < m; i++) {
-//        cout << hashTable[i][0] << " " << hashTable[i][1] << "\n";
-//    }
-//    int index = search(hashTable, 1944);
-//    cout << "index = " << index;
-//    return 0;
-//}
 
 
